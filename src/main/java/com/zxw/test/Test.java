57371766c563a8ce13eb93c1b5850c6fd5b9240e
1270153357 <1270153357@qq.com>
2020-12-30 14:46:56 +0800

    新增Test

---
+++ src/main/java/com/zxw/test/Test.java
@@ -0,0 +1,13 @@
+package com.zxw.test;
+
+/**
+ * @author : ZXW
+ * @version : 1.0
+ * @date : 2020-12-30 14:45
+ * @Description :
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("aaaaaaaaaaa");
+    }
+}
